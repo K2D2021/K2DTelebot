4bf4c29cb85c358b5a7a5df55a73322083d26f7d
K2D2021 <zxtreme@yandex.ru>
2023-04-03 22:43:17 +0300

    Add test buttons

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,9 +3,18 @@ import dev.inmo.tgbotapi.extensions.api.bot.getMe
 import dev.inmo.tgbotapi.extensions.api.bot.setMyCommands
 import dev.inmo.tgbotapi.extensions.api.send.reply
 import dev.inmo.tgbotapi.extensions.behaviour_builder.buildBehaviourWithLongPolling
+import dev.inmo.tgbotapi.extensions.behaviour_builder.expectations.waitText
 import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onCommand
+import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onPoll
+import dev.inmo.tgbotapi.requests.send.SendTextMessage
 import dev.inmo.tgbotapi.types.BotCommand
+import dev.inmo.tgbotapi.types.MenuButton
+import dev.inmo.tgbotapi.types.buttons.ReplyKeyboardMarkup
+import dev.inmo.tgbotapi.types.buttons.SimpleKeyboardButton
 import dev.inmo.tgbotapi.types.commands.BotCommandScope
+import dev.inmo.tgbotapi.utils.matrix
+import dev.inmo.tgbotapi.utils.row
+import kotlinx.coroutines.flow.first
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -15,10 +24,29 @@ suspend fun main() {
     val currentDate = sdf.format(Date())
 
     bot.buildBehaviourWithLongPolling {
-        println(getMe())
+        //println(getMe())
+        val nameReplyMarkup = ReplyKeyboardMarkup(
+            matrix {
+                row {
+                    +SimpleKeyboardButton("nope")
+                    +SimpleKeyboardButton("hope")
+                }
+            }
+        )
+
+        val menu = MenuButton
+
+
 
         onCommand("start") {
-            reply(it, "Hi :)")
+            reply(it, "Hi22 :)")
+            val name = waitText(
+                SendTextMessage(
+                    it.chat.id,
+                    "Send me your name or choose \"nope\"",
+                    replyMarkup = nameReplyMarkup
+                )
+            ).first().text.takeIf { it != "nope" }
         }
 
         onCommand("time") {
